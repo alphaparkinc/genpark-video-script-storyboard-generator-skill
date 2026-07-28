20e26eab52a3ccce44415fd618184e71e7cf0502
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-07-28 12:42:00 +0800

    feat: add client.py

---
+++ client.py
@@ -0,0 +1,5 @@
+class VideoStoryboardClient:
+    def generate_storyboard(self, concept: str) -> dict:
+        return {
+            "scenes": ['Scene 1: Problem hook', 'Scene 2: Solution reveal']
+        }
